498a2029391d4eacba48764e66e69545b2bce718
Krishna8167 <krishnasharma816757@gmail.com>
2025-06-04 19:40:05 +0530

    Finish off content in errors.go file

--- errors.go
+++ errors.go
@@ -1,52 +1,99 @@
 package main
 
-// GO programming expresses error with error values
-/* an error is any type that that implements the simple built-in error interface
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
 
-type error interface {
- Error() string
- }
+// =====================
+// ERROR HANDLING IN GO
+// =====================
 
- if something go wrong in the function, that function should return an error as its last return value
- Any code that calls a function that can return any error should handle errors by testing whether the error is 'nil'
+/*
+In Go, errors are represented by the built-in `error` type. It is an interface:
 
- //Atoi -> convertes stringified number to int
- //strconv -> string converter
+	type error interface {
+		Error() string
+	}
 
-     i, err := strconv.Atoi("42b")
-    if err != nil {
-    fmt.Println("couldn't convert:", err)
-
-      return
-     }
-
-// BECAUSE ERROR are just interfaces , you can build your own custom types that implements the error interface.
-
-type userError struct {
-name string
-}
-
-func (e UserError) Error() string {
-return fmt.Sprintf("%v has a problem with their account",e.name)
-}
-
-func sensSMS(msg, UserName string) error {
- if !canSendToUser(userName) {
-return userError{name:userName}
-}
-}
-
-// Error package
-
-the go standard library provides an "errors" package that makes it easy to deal with errors.
-
-var err error = errors.New("Something went wrong")
-
-ex:  func divide (x, y float64) (float64, error) {
- if y == 0 {
-return 0.0, errors.New("no divison by 0")
- }
-return x/y , nil
-}
-
+Functions that might fail typically return an `error` as their last return value.
+If the error is `nil`, the operation was successful; otherwise, it failed.
 */
+
+// =======================
+// Standard Error Example
+// =======================
+
+func convertStringToInt(str string) {
+	i, err := strconv.Atoi(str) // Atoi converts string to int
+	if err != nil {
+		fmt.Println("Couldn't convert:", err)
+		return
+	}
+	fmt.Println("Conversion successful:", i)
+}
+
+// =========================
+// Custom Error Definitions
+// =========================
+
+// Custom error type implementing the error interface
+type userError struct {
+	name string
+}
+
+// Error method makes userError satisfy the error interface
+func (e userError) Error() string {
+	return fmt.Sprintf("%v has a problem with their account", e.name)
+}
+
+// Dummy function to simulate whether user can receive SMS
+func canSendToUser(userName string) bool {
+	return userName != "bannedUser"
+}
+
+// Function demonstrating return of a custom error
+func sendSMS(msg, userName string) error {
+	if !canSendToUser(userName) {
+		return userError{name: userName}
+	}
+	fmt.Println("SMS sent to", userName, ":", msg)
+	return nil
+}
+
+// ===========================
+// Using errors.New Example
+// ===========================
+
+func divide(x, y float64) (float64, error) {
+	if y == 0 {
+		return 0.0, errors.New("division by zero is not allowed")
+	}
+	return x / y, nil
+}
+
+// ===========================
+// MAIN FUNCTION
+// ===========================
+
+func main() {
+	fmt.Println("1. Standard Error Example:")
+	convertStringToInt("42b") // invalid input to trigger error
+	fmt.Println()
+
+	fmt.Println("2. Custom Error Example:")
+	err := sendSMS("Hello!", "bannedUser")
+	if err != nil {
+		fmt.Println("SMS error:", err)
+	}
+	fmt.Println()
+
+	fmt.Println("3. errors.New Example:")
+	res, err := divide(10, 0)
+	if err != nil {
+		fmt.Println("Divide error:", err)
+	} else {
+		fmt.Println("Result:", res)
+	}
+}
