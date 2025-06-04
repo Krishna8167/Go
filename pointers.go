d792f5198b8337797027600fb27c0387c573273a
Krishna8167 <krishnasharma816757@gmail.com>
2025-06-04 19:58:06 +0530

    pointer concept is updated and conceptual

--- pointers.go
+++ pointers.go
@@ -1,58 +1,123 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
+// =====================================
+// POINTERS IN GO
+// =====================================
+
 /*
-a variable is a named memory location in memory that stores a value. we can manipulate the value of a variable by assigning a new
-value to it or by performing operations on it.
+A **variable** is a named location in memory that stores a value.
 
-We can manipulate the value of a variable by assigning a new value to it or by performing operations on it.
-when we assign a value in a specific location in memory.
-
-x := 42
-
-// "x" is the name of the memory. that location is stor
-
-// A pointer is a variable that stores the memory address of another variable. this means
-that a pointer "points to" the location of where the data is stored NOT the actual data itself.
-
-the  * syntax defines a pointer
-
-var p *int
-
-the & operator generates a pointer to its operand.
-
-myString := "hello"
-myStrungPtr = &myString
-
-Why are pointers useful ?
-
-pointers allows us to manipulate data in memory directly, without making copies or duplicating data. This can make programs
-more efficient and allows us to do things that would be difficult or impossible without them.
-
-// Nil pointers,
-Pointer can be dangerous
-
-if a pointer points to nothing (the zero value of the pointer type) then dereferncing it will cause a runtime error (a panic)
-that crashes the program. Generally speaking, whenever you're dealing with pointers you should check it's nil before trying
-to dereference it.
-
-func removePanic(message *string) error {
-if message == nil {
-return errors.New("invalid")
-}
-}
-
-// Pointer Receivers
-a receiver type on a method can be a pointer.const
-Method with pointer receivers can modify the value to which the receiver points. Since
-methods often need to modify their, pointer receivers are more common than value receivers.
-
-more widely used in go is a pointer receiver than a value receiver.
-
-MEthods with pointer receivers son't requires that a pointer is used to call the method. the pointer
-will automatically be derived from the value.
-
----(example)
-
-
-
+Example:
+	x := 42 // x is a variable storing the value 42
 */
+
+/*
+A **pointer** is a variable that stores the *memory address* of another variable.
+
+The `*` syntax is used to define a pointer type.
+
+	var p *int // p is a pointer to an int
+
+The `&` operator is used to get the address of a variable.
+
+	x := 10
+	ptr := &x // ptr stores the address of x
+*/
+
+func pointerBasics() {
+	x := 100
+	var ptr *int = &x
+
+	fmt.Println("Value of x:", x)
+	fmt.Println("Address of x:", &x)
+	fmt.Println("Value stored in pointer ptr:", ptr)
+	fmt.Println("Value pointed to by ptr:", *ptr) // Dereferencing
+}
+
+/*
+Pointers allow us to directly modify the value stored at a memory location.
+*/
+
+func modifyValueThroughPointer(p *int) {
+	*p = *p * 2
+}
+
+/*
+Pointers can be `nil` (i.e., they point to nothing). Dereferencing a `nil` pointer causes a runtime panic.
+So we must check before accessing it.
+*/
+
+func safeAccess(s *string) error {
+	if s == nil {
+		return errors.New("cannot access value: pointer is nil")
+	}
+	fmt.Println("Value:", *s)
+	return nil
+}
+
+/*
+POINTER RECEIVERS
+
+Methods can use value or pointer receivers.
+
+- Value Receiver: operates on a copy (doesn't affect the original)
+- Pointer Receiver: operates on the original value (can modify it)
+*/
+
+type Counter struct {
+	value int
+}
+
+// Value receiver (does NOT modify the original)
+func (c Counter) IncrementByVal() {
+	c.value++
+}
+
+// Pointer receiver (modifies the original)
+func (c *Counter) IncrementByPtr() {
+	c.value++
+}
+
+// This works even if you call pointer receiver on a value; Go handles it automatically
+
+func receiverDemo() {
+	c := Counter{value: 5}
+
+	c.IncrementByVal()
+	fmt.Println("After IncrementByVal:", c.value) // Output: 5 (unchanged)
+
+	c.IncrementByPtr()
+	fmt.Println("After IncrementByPtr:", c.value) // Output: 6
+}
+
+// ===========================
+// MAIN FUNCTION
+// ===========================
+
+func main() {
+	fmt.Println("1. Pointer Basics")
+	pointerBasics()
+
+	fmt.Println("\n2. Modify Through Pointer")
+	num := 50
+	modifyValueThroughPointer(&num)
+	fmt.Println("Modified value:", num) // Output: 100
+
+	fmt.Println("\n3. Nil Pointer Check")
+	var str *string = nil
+	err := safeAccess(str)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
+	msg := "hello"
+	err = safeAccess(&msg)
+
+	fmt.Println("\n4. Pointer Receivers vs Value Receivers")
+	receiverDemo()
+}
